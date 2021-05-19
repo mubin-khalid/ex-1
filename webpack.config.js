38ed9fe01f096518643bbb596dc76cf17a7d7bbd
Mubin <mubin@Mubins-MacBook-Pro.local>
2021-05-19 15:32:11 +0500

    boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.jsx"),
+  output: { path: path.join(__dirname, "build"), filename: "index.[hash].js" },
+  mode: process.env.NODE_ENV || "development",
+  resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
+  devServer: { contentBase: path.join(__dirname, "src") },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", 'eslint-loader']
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(less)$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+          {
+            loader: "less-loader", // compiles Less to CSS
+            options: {
+              lessOptions: {
+                modifyVars: {
+                  'primary-color': '#1DA57A',
+                  'link-color': '#1DA57A',
+                  'border-radius-base': '2px',
+                },
+                javascriptEnabled: true,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"]
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
