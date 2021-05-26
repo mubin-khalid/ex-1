038e10370fb3fc447b5365ad589b8407f79c96ed
mubin-khalid <mbnmughal30@gmail.com>
2021-05-26 21:58:46 +0500

    update combine reducer to use the reducers.

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import userReducer from './userReducer';
+import settingsReducer from './settingsReducer';
 
 export default combineReducers({
-  dummy: [],
+  userState: userReducer,
+  settingsState: settingsReducer,
 });
